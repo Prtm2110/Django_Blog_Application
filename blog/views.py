fef7362fb2e99fad43c9a3d1ca8eccf817ef596c
Prtm2110 <prathamhole@gmail.com>
2024-10-26 01:32:51 +0530

    added base.html to reduce redudency

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'title' : 'blog 1',
+        'post_date' : '12-45-75',
+        'content' : 'this is my first blog'
+    },
+        {
+        'title' : 'blog 2',
+        'post_date' : '55412-75',
+        'content' : 'this is my second blog'
+    }
+]
+
 # Create your views here.
 
 def home(request):
-    return render(request,'blog/home.html')
+    context = { 
+        'posts' :posts
+    }
+    return render(request,'blog/home.html',context)
 
 def about(request):
-    return render(request,'blog/about.html')
+    return render(request,'blog/about.html' , {'title' : 'about'})
