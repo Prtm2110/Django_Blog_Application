aa28386680046a7d99fccdef2f309ef62f3bd211
Prtm2110 <prathamhole@gmail.com>
2024-10-30 21:28:45 +0530

    profile view and redirect to login if proflie wasnt logged in

--- django_project/urls.py
+++ django_project/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'), 
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile/', user_views.profile, name='profile'), 
     #empty becuase now we can direclty go to /home and /about rather than blog/home and blog/about
 ]
